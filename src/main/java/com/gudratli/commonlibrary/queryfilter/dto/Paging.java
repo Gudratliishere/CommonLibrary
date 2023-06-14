450c2203ee6a91facabe85543551925b77770dc6
Dunay Gudratli <dqudretli@std.beu.edu.az>
2023-06-14 14:09:20 +0400

    adding static factory method to Paging

--- src/main/java/com/gudratli/commonlibrary/queryfilter/dto/Paging.java
+++ src/main/java/com/gudratli/commonlibrary/queryfilter/dto/Paging.java
@@ -13,4 +13,11 @@ public class Paging
 {
     private int pageNo;
     private int pageSize;
+
+    public static Paging of (final Integer pageNo, final Integer pageSize)
+    {
+        if (pageNo != null && pageSize != null)
+            return new Paging(pageNo, pageSize);
+        return null;
+    }
 }
