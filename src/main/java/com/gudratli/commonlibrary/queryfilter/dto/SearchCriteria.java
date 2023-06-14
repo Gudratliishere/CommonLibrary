5eb79980c0f508b12b6cbb79e14d852bf7436be2
Dunay Gudratli <dqudretli@std.beu.edu.az>
2023-06-14 14:01:12 +0400

    adding all args constructor and builder to models

--- src/main/java/com/gudratli/commonlibrary/queryfilter/dto/SearchCriteria.java
+++ src/main/java/com/gudratli/commonlibrary/queryfilter/dto/SearchCriteria.java
@@ -1,8 +1,14 @@
 package com.gudratli.commonlibrary.queryfilter.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SearchCriteria
 {
     private String key;
--- src/main/java/com/gudratli/commonlibrary/queryfilter/dto/SearchFilter.java
+++ src/main/java/com/gudratli/commonlibrary/queryfilter/dto/SearchFilter.java
@@ -1,10 +1,16 @@
 package com.gudratli.commonlibrary.queryfilter.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SearchFilter
 {
     private List<SearchCriteria> searchCriteriaList;
