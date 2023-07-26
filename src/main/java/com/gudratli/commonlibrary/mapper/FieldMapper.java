5a8dadc2813f5badeb9cac0bb6794fd7ba32a3e2
Dunay Gudratli <dqudretli@std.beu.edu.az>
2023-07-26 17:05:24 +0400

    Field mapper null pointer exception handled

--- src/main/java/com/gudratli/commonlibrary/mapper/FieldMapper.java
+++ src/main/java/com/gudratli/commonlibrary/mapper/FieldMapper.java
@@ -1,5 +1,6 @@
 package com.gudratli.commonlibrary.mapper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
@@ -12,19 +13,26 @@ import java.util.Map;
  * @since : 20.07.2023
  **/
 @Component
+@Slf4j
 public class FieldMapper
 {
     public <T> void map (Map<Object, Object> source, T target)
     {
         source.forEach((key, value) ->
         {
-            Field field = ReflectionUtils.findField(target.getClass(), (String) key);
-            assert field != null;
-            field.setAccessible(true);
-            if (field.getType() == Double.class)
-                ReflectionUtils.setField(field, target, Double.valueOf(value.toString()));
-            else
-                ReflectionUtils.setField(field, target, value);
+            try
+            {
+                Field field = ReflectionUtils.findField(target.getClass(), (String) key);
+                assert field != null;
+                field.setAccessible(true);
+                if (field.getType() == Double.class)
+                    ReflectionUtils.setField(field, target, Double.valueOf(value.toString()));
+                else
+                    ReflectionUtils.setField(field, target, value);
+            } catch (NullPointerException ex)
+            {
+                log.error("'" + key + "' field is not found, skipping..");
+            }
         });
     }
 }
